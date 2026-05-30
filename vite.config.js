4e8c76b6221dc0627dbb3bc2d6af26665ee77744
Aman Kumar <amankumar3432k@gmail.com>
2026-05-30 12:37:05 +0530

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Change to '/your-repo-name/' for GitHub Pages subdirectory deploy
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'terser',
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
+plugins: [react()],
+base: '/FUTURE_FS_01/',
+
+build: {
+outDir: 'dist',
+sourcemap: false,
+minify: 'terser',
+},
+
+server: {
+port: 3000,
+open: true,
+},
 })
